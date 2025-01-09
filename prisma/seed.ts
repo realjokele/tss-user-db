c98d588fc327be5f1fc44833581b7ee3fec98b2b
realjokele <misc@jokele.de>
2025-01-09 20:29:10 +0100

    Initial commit

---
+++ prisma/seed.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+import { faker } from '@faker-js/faker'
+
+const prisma = new PrismaClient()
+
+await prisma.user.deleteMany()
+
+Array.from({ length: 1000 }).map(async (_, i) => {
+	const firstName = faker.person.firstName()
+	const lastName = faker.person.lastName()
+
+	await prisma.user.create({
+		data: {
+			email: faker.internet.email({
+				firstName,
+				lastName,
+			}),
+			name: `${firstName} ${lastName}`,
+		},
+	})
+})
